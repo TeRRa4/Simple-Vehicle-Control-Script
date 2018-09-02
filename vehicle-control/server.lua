09f691b1d41c58d219d9b9298de9e67c9876db63
TeRRa4 <bevan.edwards93@gmail.com>
2018-09-02 11:47:07 +0100

    Remove lock, save and save toggle
    This is so that the functionality of this plugin doesn't clash with other installed plugins on Midgard RP

--- vehicle-control/server.lua
+++ vehicle-control/server.lua
@@ -42,30 +42,3 @@ AddEventHandler('chatMessage', function(s, n, m)
 		TriggerClientEvent('hood', s)
 	end
 end)
--- L O C K --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/lock" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('lock', s)
-	end
-end)
--- S A V E --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/save" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('save', s)
-	end
-end)
--- R E M O T E --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/sveh" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('controlsave', s)
-	end
-end)
